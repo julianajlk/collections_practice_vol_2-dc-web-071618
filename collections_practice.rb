0d08f3b19eb46fe7b30fa306de0f732843d33a55
Juliana Kang <julianajlk@gmail.com>
2018-07-13 05:14:28 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -23,7 +23,7 @@ end
 def merge_data(keys, data)
   keys.each do |name_hash|
     data.each do |hash|
-      name_hash.merge(hash[name_hash[:first_name]])
+      name_hash.merge!(hash[name_hash[:first_name]])
     end
   end
 end
