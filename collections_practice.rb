7703a710e60a1d2f763137062c35b96c1f5bf1ae
Juliana Kang <julianajlk@gmail.com>
2018-07-13 05:12:04 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -10,3 +10,12 @@ end
 def remove_non_strings(array)
   array.reject{|element| element.class !=String}
 end
+
+def count_elements(array)
+  counts = Hash.new(0)
+  array.collect {|element| counts[element]+=1 }
+    counts.collect do |hash, number|
+      hash[:count] = number
+    end
+  counts.keys
+end
